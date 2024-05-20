f06356eebc1968860a987789f441e711627159b7
Dannick Bedard <dannickbedard@MacBook-Air-de-Marianne-2.local>
2024-05-19 20:17:15 -0400

    Try to build and test.

---
+++ dist/components/MyComponent.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+// src/MyComponent.jsx
+var MyComponent = function MyComponent(_ref) {
+  var name = _ref.name;
+  return /*#__PURE__*/React.createElement("div", null, "Hello, ", name, "!");
+};
+var _default = exports["default"] = MyComponent;
---
+++ main.js
@@ -0,0 +1,6 @@
+// main.js
+import MyComponent from './dist/components/MyComponent';
+
+const element = MyComponent({ name: 'World' });
+document.body.appendChild(element);
+
